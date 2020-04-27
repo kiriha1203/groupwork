1acf57991e2e9a5572de227d79eee3632fe84e38
katsuhiro.tsukinoki@gmail.com <slowandlow3104>
2020-04-27 17:44:42 +0900

    Add user-show,edit,update追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,10 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy] 
+
   def new
     @user = User.new
   end
+
   def show
     @user = User.find(params[:id])
   end
@@ -14,5 +17,24 @@ class UsersController < ApplicationController
       render :new
     end
   end
-  
+
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:notice] = "ユーザー情報を編集しました"
+      redirect_to user_path(@user.id)
+    else
+      render("users/edit")
+    end
+  end
+
+  private
+  def user_params
+      params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  def user_choose_new
+    if action_name == 'new'
+      confirm_users_path
+    elsif action_name == 'edit'
+      user_path
+    end
+  end
 end
